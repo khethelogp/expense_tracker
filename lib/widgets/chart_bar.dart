54998579997eef1181ef65c9e52da63f99596534
Khethelo Ndhlala <khethelogp@gmail.com>
2021-12-20 16:59:25 +0200

    media queries and different screens

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -10,17 +10,20 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return LayoutBuilder(builder: (ctx, constraints){
+      return Column(
       children: [
         Container(
-          height: 20,
+          height: constraints.maxHeight * 0.15,
           child: FittedBox(
             child: Text('\$${spendingAmount.toStringAsFixed(0)}')
           ),
         ),
-        SizedBox(height: 4,),
+        SizedBox(
+          height: constraints.maxHeight * 0.05,
+        ),
         Container(
-          height: 60,
+          height: constraints.maxHeight * 0.6,
           width: 10,
           child: Stack(
             children: [
@@ -43,9 +46,18 @@ class ChartBar extends StatelessWidget {
             ],
           ),
         ),
-        SizedBox(height:4),
-        Text(label),
+        SizedBox(
+          height: constraints.maxHeight * 0.05,
+        ),
+        Container(
+          height: constraints.maxHeight * 0.15,          
+          child: FittedBox(
+              child: Text(label)
+          )
+        ),
       ],
     );
+    },);
+    
   }
 }
